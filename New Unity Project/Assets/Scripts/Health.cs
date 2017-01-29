7047e65e2fce60fbf36a422b737a9686c0d68703
Eminda <eminda.14@cse.mrt.ac.lk>
2017-01-30 00:49:18 +0530

    Working-bug fixed coin searchin

---
+++ New Unity Project/Assets/Scripts/Health.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Health : MonoBehaviour {
+
+    // Use this for initialization
+    float timeLeft;
+    bool b = true;
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (!b)
+        
+        {
+            timeLeft -= Time.deltaTime * 1000;
+            if (timeLeft < 0)
+            {
+                UnityEngine.Debug.logger.Log("Health  vanished "  + "  time" + Time.time);
+                Destroy(gameObject);
+            }
+        }
+    }
+    public void setValues(int timeLeft)
+    {
+        this.timeLeft = timeLeft;
+        b = false;
+        Debug.logger.Log("HValues set"+Time.time);
+    }
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Tank")
+        {
+            col.gameObject.SendMessage("healthGained");
+            Vector3 pos = transform.position;
+            int x = (int)pos.x;
+            int y = -(int)pos.y;
+            ServerListener.serverListener.map[x, y] = null;
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ New Unity Project/Assets/Scripts/Wall.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wall : MonoBehaviour {
+
+    // Use this for initialization
+    public GameObject next;
+
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        Debug.logger.Log("colided"+ col.gameObject.tag);
+        if (col.gameObject.tag == "bullet")
+        {
+            Vector3 position = transform.position;
+            Quaternion rotation = transform.rotation;
+            Destroy(gameObject);
+            Vector3 pos = transform.position;
+            int x = (int)pos.x;
+            int y = -(int)pos.y;
+            if (next != null)
+            {
+                Instantiate(next, position, rotation);
+                
+
+                string brick=ServerListener.serverListener.map[x, y];
+                int val = int.Parse(brick.Substring(1, 1));
+                brick = brick.Substring(0, 1) + (val - 1);
+                ServerListener.serverListener.map[x, y] = brick;
+                Debug.logger.Log("colided opa");
+            }
+            else
+            {
+                ServerListener.serverListener.map[x, y] = null;
+            }
+        }
+    }
+}
